c1bf7b1e83bbbc77add56ff5ede73b8b6aff2e1d
Jonathan Sweemer <sweemer@gmail.com>
2011-12-01 21:30:36 +0900

    Added support for credit card payments

--- Maishoku/Maishoku/CheckoutViewController.h
+++ Maishoku/Maishoku/CheckoutViewController.h
@@ -9,18 +9,24 @@
 #import <RestKit/RestKit.h>
 #import <UIKit/UIKit.h>
 
-@interface CheckoutViewController : UIViewController <RKRequestDelegate>
+@interface CheckoutViewController : UIViewController <RKRequestDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (weak, nonatomic) IBOutlet UIButton *confirmOrderButton;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @property (weak, nonatomic) IBOutlet UILabel *confirmedLabel;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
+@property (weak, nonatomic) IBOutlet UITextField *cardNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *expirationDateTextField;
+@property (weak, nonatomic) IBOutlet UITextField *securityCodeTextField;
+@property (weak, nonatomic) IBOutlet UILabel *expirationDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *securityCodeLabel;
 
 - (IBAction)valueChanged:(id)sender;
-
 - (IBAction)confirmOrder:(id)sender;
-
 - (IBAction)done:(id)sender;
+- (IBAction)cardNumberEditingChanged:(id)sender;
+- (IBAction)expirationDateEditingChanged:(id)sender;
+- (IBAction)securityCodeEditingChanged:(id)sender;
 
 @end
--- Maishoku/Maishoku/CheckoutViewController.m
+++ Maishoku/Maishoku/CheckoutViewController.m
@@ -19,19 +19,41 @@
 @synthesize spinner;
 @synthesize confirmedLabel;
 @synthesize doneButton;
+@synthesize cardNumberTextField;
+@synthesize expirationDateTextField;
+@synthesize securityCodeTextField;
+@synthesize expirationDateLabel;
+@synthesize securityCodeLabel;
+
+- (void)setButtonStatus
+{
+    if (UIAppDelegate.paymentMethod == credit_card
+      && (12  > [cardNumberTextField.text length]
+        || 4 != [expirationDateTextField.text length]
+        || 3  > [securityCodeTextField.text length]
+    )) {
+        [confirmOrderButton setEnabled:NO];
+        [confirmOrderButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    } else {
+        [confirmOrderButton setEnabled:YES];
+        [confirmOrderButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [segmentedControl setEnabled:NO forSegmentAtIndex:1];
     [segmentedControl setTitle:NSLocalizedString(@"Cash", nil) forSegmentAtIndex:0];
     [segmentedControl setTitle:NSLocalizedString(@"Credit Card", nil) forSegmentAtIndex:1];
     [confirmOrderButton setTitle:NSLocalizedString(@"Confirm Order", nil) forState:UIControlStateNormal];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
+    [cardNumberTextField setPlaceholder:NSLocalizedString(@"Credit Card Number", nil)];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YY"];
+    NSTimeInterval oneYear = 60 * 60 * 24 * 365;
+    NSString *date = [formatter stringFromDate:[[NSDate date] dateByAddingTimeInterval:oneYear]];
+    formatter = nil;
+    [expirationDateTextField setPlaceholder:[NSString stringWithFormat:@"09%@", date]];
+    [securityCodeTextField setPlaceholder:@"123"];
     [self valueChanged:nil];
 }
 
@@ -39,9 +61,20 @@
 {
     if ([segmentedControl selectedSegmentIndex] == 0) {
         UIAppDelegate.paymentMethod = cash_on_delivery;
+        [cardNumberTextField setHidden:YES];
+        [expirationDateTextField setHidden:YES];
+        [securityCodeTextField setHidden:YES];
+        [expirationDateLabel setText:nil];
+        [securityCodeLabel setText:nil];
     } else {
         UIAppDelegate.paymentMethod = credit_card;
+        [cardNumberTextField setHidden:NO];
+        [expirationDateTextField setHidden:NO];
+        [securityCodeTextField setHidden:NO];
+        [expirationDateLabel setText:NSLocalizedString(@"Expiration Date", nil)];
+        [securityCodeLabel setText:NSLocalizedString(@"Security Code", nil)];
     }
+    [self setButtonStatus];
 }
 
 - (IBAction)confirmOrder:(id)sender
@@ -51,9 +84,13 @@
     [spinner startAnimating];
     
     /*
-     * The JSON message should look like this:
+     * The JSON message should look like this for cash payments:
      * 
-     * {'restaurant_id': 9, 'payment_method': 1, 'is_delivery': True, 'items': [{'item_id': 21, 'quantity': 1}, {'item_id': 22, 'quantity': 2}]})
+     * {"restaurant_id": 9, "payment_method": 2, "is_delivery": false, "items": [{"item_id": 21, "quantity": 1}, {"item_id": 22, "quantity": 2}]}
+     *
+     * And like this for credit card payments:
+     * 
+     * {"restaurant_id": 9, "payment_method": 1, "is_delivery": false, "items": [{"item_id": 21, "quantity": 1}, {"item_id": 22, "quantity": 2}], "card_number": "4111111111111111", "expiration_date": "12/13", "security_code": "1234"}
      */
 
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:4];
@@ -74,6 +111,12 @@
     [dict setObject:[NSNumber numberWithInt:UIAppDelegate.paymentMethod] forKey:@"payment_method"];
     [dict setObject:[NSNumber numberWithBool:UIAppDelegate.orderMethod] forKey:@"is_delivery"];
     
+    if (UIAppDelegate.paymentMethod == credit_card) {
+        [dict setObject:cardNumberTextField.text forKey:@"card_number"];
+        [dict setObject:expirationDateTextField.text forKey:@"expiration_date"];
+        [dict setObject:securityCodeTextField.text forKey:@"security_code"];
+    }
+    
     id<RKParser> parser = [[RKParserRegistry sharedRegistry] parserForMIMEType:RKMIMETypeJSON];
     NSString *json = [parser stringFromObject:dict error:nil];
     NSObject<RKRequestSerializable> *params = [RKRequestSerialization serializationWithData:[json dataUsingEncoding:NSUTF8StringEncoding] MIMEType:RKMIMETypeJSON];
@@ -86,6 +129,62 @@
     [self dismissModalViewControllerAnimated:YES];
 }
 
+- (IBAction)cardNumberEditingChanged:(id)sender
+{
+    [self setButtonStatus];
+}
+
+- (IBAction)expirationDateEditingChanged:(id)sender
+{
+    [self setButtonStatus];
+}
+
+- (IBAction)securityCodeEditingChanged:(id)sender
+{
+    [self setButtonStatus];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    [cardNumberTextField resignFirstResponder];
+    [expirationDateTextField resignFirstResponder];
+    [securityCodeTextField resignFirstResponder];
+}
+
+/*------------------------------------------------------------------------------------*/
+/* UITextFieldDelegate                                                                */
+/*------------------------------------------------------------------------------------*/
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self setButtonStatus];
+    
+    if (textField == cardNumberTextField
+     || textField == expirationDateTextField
+     || textField == securityCodeTextField
+    ) {
+        [textField resignFirstResponder];
+    }
+    
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSUInteger length = [string length] - range.length;
+    
+    if (textField == cardNumberTextField) {
+        return [textField.text length] + length <= 20;
+    } else if (textField == expirationDateTextField) {
+        return [textField.text length] + length <= 4;
+    } else if (textField == securityCodeTextField) {
+        return [textField.text length] + length <= 4;
+    }
+    
+    return YES;
+}
+
 /*------------------------------------------------------------------------------------*/
 /* RKRequestDelegate                                                                  */
 /*------------------------------------------------------------------------------------*/
@@ -95,28 +194,36 @@
     [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Order Failed", nil) message:NSLocalizedString(@"Contact Support", nil) delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil] show];
 }
 
-- (void)restoreButton
+- (void)lockDown
 {
     [spinner stopAnimating];
+    [Cart clear];
     [confirmOrderButton setTitle:NSLocalizedString(@"Confirm Order", nil) forState:UIControlStateNormal];
     [confirmOrderButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [confirmOrderButton setEnabled:NO];
+    [doneButton setEnabled:YES];
+    [segmentedControl setEnabled:NO];
+    [cardNumberTextField setText:nil];
+    [cardNumberTextField setEnabled:NO];
+    [expirationDateTextField setText:nil];
+    [expirationDateTextField setEnabled:NO];
+    [securityCodeTextField setText:nil];
+    [securityCodeTextField setEnabled:NO];
 }
 
-- (void)request:(RKRequest*)request didLoadResponse:(RKResponse *)response
+- (void)request:(RKRequest *)request didLoadResponse:(RKResponse *)response
 {
-    [self restoreButton];
+    [self lockDown];
     if ([response isCreated]) {
         [confirmedLabel setText:[NSString stringWithFormat:@"%@\n%@.", NSLocalizedString(@"Order Confirmed", nil), UIAppDelegate.restaurant.deliveryTime]];
-        [Cart clear];
-        [doneButton setEnabled:YES];
     } else {
         [self showAlert];
     }
 }
 
-- (void)request:(RKRequest*)request didFailLoadWithError:(NSError*)error
+- (void)request:(RKRequest *)request didFailLoadWithError:(NSError *)error
 {
-    [self restoreButton];
+    [self lockDown];
     [self showAlert];
 }
 
@@ -131,6 +238,11 @@
     [self setSpinner:nil];
     [self setConfirmedLabel:nil];
     [self setDoneButton:nil];
+    [self setCardNumberTextField:nil];
+    [self setExpirationDateTextField:nil];
+    [self setSecurityCodeTextField:nil];
+    [self setExpirationDateLabel:nil];
+    [self setSecurityCodeLabel:nil];
     [super viewDidUnload];
 }
 
